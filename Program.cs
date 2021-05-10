6011f1f49cb3897d6d329470fb95e7aa5f699367
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2021-05-10 03:19:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle;
+            int adet = 0;
+            
+            string sesli = "a,e,i,ı,o,ö,u,ü";
+            Console.Write("Bir cümle giriniz :");
+            cumle = Console.ReadLine();
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                for (int k = 0; k < cumle.Length; k++)
+                {
+                    if (cumle[i] == sesli[k])
+                    {
+                        adet++;
+                    }
+                }
+                
+            }
+            Console.WriteLine("Sesli harf sayısı :{0}", adet);
+            Console.ReadKey();
+        }
+    }
+}
